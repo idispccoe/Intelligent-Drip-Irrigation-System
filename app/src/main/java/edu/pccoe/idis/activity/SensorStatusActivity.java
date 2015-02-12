df688c7223e76c384af30394ce1b1d425e5030ac
prashantpatil92 <prashant.patil9000@gmail.com>
2015-02-12 12:04:16 +0530

    Reading all sensor values

--- app/src/main/java/edu/pccoe/idis/activity/SensorStatusActivity.java
+++ app/src/main/java/edu/pccoe/idis/activity/SensorStatusActivity.java
@@ -4,16 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import edu.pccoe.idis.R;
+import edu.pccoe.idis.utils.HttpRequestTask;
+import edu.pccoe.idis.utils.ServerResponse;
 
 
 public class SensorStatusActivity extends ActionBarActivity {
 
+    private TextView sensorStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_status);
+        sensorStatus = (TextView) findViewById(R.id.textView4);
+        new ServerRequestTask().execute("sensorStatus");
     }
 
 
@@ -37,5 +45,12 @@ public class SensorStatusActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ServerRequestTask extends HttpRequestTask {
+        @Override
+        protected void onPostExecute(ServerResponse res) {
+            sensorStatus.setText(res.getContent());
+        }
     }
 }
